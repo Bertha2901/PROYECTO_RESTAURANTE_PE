93028f11cc6d965c4840750a5388251f3b98ed36
Juanrf <juanr_fuentes@hotmail.com>
2020-07-30 17:03:03 -0600

    arreglos y matrices

--- src/proyectorestaurante.cpp
+++ src/proyectorestaurante.cpp
@@ -5,80 +5,30 @@
 using namespace std;
 
 int menu(){
-    int opcion, contador=0, Detalle=0, TotalAcumulador=0, acumulador=0, cantidad, Tpagar, menu1=200, menu2=150, menu3=110;
-    char seguir;
+    int compras, opciones, total, subtotal, isv;
     double const ISV=0.15;
-    do
+    char seguir;
+    string arreglo[5]{"Langosta","Pescado Frito","Sopa de caracol","Almejas","Filete"};
+    int precio[5]{150,200,160,250,140};
+    cout<<"Bienvenido a Restaurante DELFIN porfavor elija el menu del dia: "<<endl;
+    cout<<endl;
+    for (int i = 0; i < 5; i++)
     {
-        cout<<"A continuacion se le presenta el menu de la comida elija lo que sea de su gusto"<<endl;
-        cout<<"[1]-LANGOSTA --------------------LPS 200.00"<<endl;
-        cout<<"[2]-CAMARONES AL AJILLO----------LPS 150.00"<<endl;
-        cout<<"[3]-FILETE DE PESCADO------------LPS 110.00"<<endl;
-        cin >>opcion;
-        switch (opcion)
-        {
-        case 1:
-            cout<<"El plato viene incluido con arroz, tajadas, encurtido refresco"<<endl;
-            cout<<"El precio del plato es de "<<menu1<<" Lempiras "<<endl;
-            cout<<"Cuantas LANGOSTAS desea ordenar"<<endl;
-            cin >>cantidad;
-            cout<<"Desea seguir ordenando, o avanzar a la cancelacion de su pedido s/n?";
-            cin >>seguir;
-            contador++;
-            acumulador=menu1*cantidad;
-            break;
-        case 2:
-            cout<<"El plato viene incluido con tostones, ensalada de lechuga, arroz y refresco"<<endl;
-            cout<<"El precio del plato es de "<<menu2<<" Lempiras "<<endl;
-            cout<<"Cuantos camarones desea ordenar"<<endl;
-            cin >>cantidad;
-            cout<<"Desea seguir ordenando, o avanzar a la cancelacion de su pedido s/n?";
-            cin >>seguir;
-            contador++;
-            acumulador=menu2*cantidad;
-            break;
-        case 3:
-            cout<<"El plato viene incluido con tajadas, rise and beans, encurtido y refresco"<<endl;
-            cout<<"El precio del plato es de "<<menu3<<" Lempiras "<<endl;
-            cout<<"Cuantos filetes desea ordenar"<<endl;
-            cin >>cantidad;
-            cout<<"Desea seguir ordenando, o avanzar a la cancelacion de su pedido s/n?";
-            cin >>seguir;
-            contador++;
-            acumulador=menu3*cantidad;
-            break;
-        default:
-            break;
-        }
-        cout<<Factura()<<endl;
-    } while (seguir!='n');
-     return 0;
-}
-int Factura(){
-    int Tpagar, Detalle, TotalAcumulador, contador, acumulador,ISV;
-    TotalAcumulador+=acumulador;
-    Detalle=TotalAcumulador*ISV;
-    Tpagar=Detalle+TotalAcumulador;
-    cout<<"Ha hecho la compra de "<<contador<<" Ordenes"<<endl;
-    cout<<"el Subtotal a pagar es de :"<<TotalAcumulador<<" Lempiras"<< endl;
-    cout<<" ISV: "<<Detalle<<endl;
-    cout<<"El total a pagar de la factura es: "<<Tpagar<<endl;
+        cout<<i+1<<"."<<arreglo[i]<<"---Valor--->"<<" Lps"<<precio[i]<<endl;
+    }
+    cout<<"Elija cuantos platos quiere hoy para su menu: "<<endl;
+    cin >>opciones;
+    for (int i = 0; i < opciones; i++)
+    {
+        cout<<"Por favor ingrese las opciones para el dia de hoy: "<<endl;
+        cin >>arreglo[i];
+    }
     return 0;
-}
+}   
 int main(){
     int opcion, Nmesa, platillos, saludo;
     string nombre;
-    int Nmesas[10];  //Implementacion del arreglo de mesas
-    Nmesas[0]=1;
-    Nmesas[1]=2;
-    Nmesas[2]=3;
-    Nmesas[3]=4;
-    Nmesas[4]=5;
-    Nmesas[5]=6;
-    Nmesas[6]=7;
-    Nmesas[7]=8;
-    Nmesas[8]=9;
-    Nmesas[9]=10;
+    string Nmesas[6]{"Mesa 1","Mesa 2","Mesa 3","Mesa 4","Mesa 5","Mesa6 "};
     cout<<endl;
     cout<<"          ==================================================          "<<endl;
     cout<<"                  BIENVENIDO A RESTAURANTE EL DELFIN                  "<<endl;
@@ -95,7 +45,7 @@ int main(){
             cin >>nombre;
             if (nombre=="bertha")
             {
-                cout<<"La reservacion esta a nombre de bertha, la mesa asignada es:"<<Nmesas[8]<<endl;
+                cout<<"La reservacion esta a nombre de bertha, la mesa asignada es:"<<Nmesas[6][4]<<endl;
             }
             else
             {
@@ -110,7 +60,6 @@ int main(){
                 {
                     cout<<"Mesa "<<Nmesas[i]<<"    Si esta Disponible"<<endl;   
                 }
-                
            cout<<"Las mesas no disponiles son: "<<endl;
               for (int i =5; i < 10; i++)
 
@@ -124,11 +73,10 @@ int main(){
                     {
                         cout<<"solicite una nueva mesa"<<endl;
                     }
-                    
                 }
-   
            cout<<"Que numero de mesa desea: ";
-           cin >>Nmesa;
+           cin >>Nmesas[6];
+           cout<<"Elija el menu de su gusto";
            cout<<menu()<<endl;
            break;
 
