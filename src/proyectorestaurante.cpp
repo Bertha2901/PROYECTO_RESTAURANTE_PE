bab3e62505ef4c05512c7de8e749592e4509fd16
Bertha <berthaturcios3@gmail.com>
2020-08-05 20:52:09 -0600

    funcion vender y facturacion

--- src/proyectorestaurante.cpp
+++ src/proyectorestaurante.cpp
@@ -45,7 +45,82 @@ int reservacion(){
     cout<<endl;
     return 0;
 }
+void menuDia(string[matrizMenu],string[menu][precio]){
 
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<menu[i]<<" valor LPS."<<precio[i]<<endl;
+    }
+}
+
+int Vender(){
+    menuDia();
+    int cantidad;
+    string menu[3];
+    cout<<"Cuantos platillos ordenara:"<<endl;
+    cin>>cantidad;
+    for (int i = 0; i < cantidad; i++)
+    {
+        cout<<"Ingrese el nombre del platillo "<<endl;
+        cin>>menu[i];
+    }
+    cout<<"Por favor confirme que esta es su orden final y precione ENTER"<<endl;
+    for (int i = 0; i < cantidad; i++)
+    {
+        cout<<menu[i]<<endl;
+    }
+    
+    cout<<"Espere 15 minutos mientras sale su orden"<<endl;
+    cout<<"Aqui esta su orden"<<endl;
+    cout<<"Buen Provecho!!"<<endl;
+   
+	return 0;
+}
+int facturacion(){
+    int numero,Nordenes,factura;
+    string menu[5];
+    int precio[5];
+    cout<<"Ingrese el numero de factura:";
+    cin>>numero;
+    cout<<"Ingrese la cantidad de ordenes que pidio el cliente:"<<endl;
+    cin>>Nordenes;
+    for (int i = 0; i < Nordenes; i++)
+    {
+        cout<<"Ingrese  el nombre de los platillos que consumio el cliente:"<<endl;
+        cin>>menu[i];
+        cout<<"Ingrese el precio que corresponde a ese platillo"<<endl;
+        cin>>precio[i];
+ 
+    }
+     cout<<"El cliente consumio:"<<endl;
+    for (int i = 0; i < Nordenes; i++)
+    {
+        cout<<menu[i]<<" con valor de" <<precio[i]<<endl;
+    }
+    cout<<"El total a pagar por el consumo hecho es de:"<<factura<<endl;
+    cout<<"Presione ENTER para imprimir la factura al cliente"<<endl;
+
+    return 0;
+}
+int reservacion(){
+    string dias[3];
+    int i;
+    cout<<"Bienvenido a la reservacion para restaurante el DELFIN"<<endl;
+    cout<<endl;
+    cout<<"Detalle que dias desea poner disponible para reservaciones "<<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cin>>dias[i];   
+    }
+    cout<<endl;
+    cout<<"Los dias disponibles son: "<<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<dias[i]<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
 
 int main(){
     int opcion;
@@ -68,7 +143,10 @@ int main(){
             cout<<reservacion();
             break;
         case 3:
-            cout<<endl;
+           cout<<Vender();
+            break;
+        case 4:
+            cout<<facturacion();
             break;
         default:
         cout<<"Esta opcion no esta disponible"<<endl;
