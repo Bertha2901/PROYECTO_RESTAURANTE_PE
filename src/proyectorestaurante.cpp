626a7d8b72fa05a966e7699b74f7b86a2dfd6a15
Juanrf <juanr_fuentes@hotmail.com>
2020-08-09 22:15:57 -0600

    estructuras

--- src/proyectorestaurante.cpp
+++ src/proyectorestaurante.cpp
@@ -4,26 +4,38 @@
 #include <math.h>
 #include <stdlib.h>
 using namespace std;
-int matrizMenu(){
-    string menu[5];
+
+struct platillos
+{
+    string Menu[5];
     int precio[5];
-    cout<<"A continuacion ingrese el nombre de los platos que desea mostrar y seguido su valor en LPS : "<<endl;
+}m1;
+void pedirmenu();
+void mostrarmenu(platillos);
+
+void pedirmenu(){
+    cout<<"Ingrese el nombre de los platos y seguido su valor: "<<endl;
     for (int i = 0; i < 5; i++)
     {
-        cin>>menu[i]>>precio[i];
+        cout<<"plato #"<<i+1<<endl;
+        cin>>(m1. Menu[i]);
+        cout<<"Precio "<<endl;
+        cin>>(m1. precio[i]);
     }
-    cout<<endl;
-    cout<<"Los platos a presentar este dia son: "<<endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cout<<menu[i]<<" valor LPS."<<precio[i]<<endl;
-    }
-    return 0;
 }
-void menuDia(string menu[], int precio[]){// recibir parametros
-        for (int i = 0; i < 5; i++)
+void mostrarmenu(platillos m){
+    int cantidad, acumulador, opcion;
+    string nombre;
+    for (int i = 0; i < 5; i++)
     {
-        cout<<menu[i]<<" valor LPS."<<precio[i]<<endl;
+        cout<<m.Menu[i]<<" valor "<<m.precio[i]<<endl;
+    }
+    cout<<"Ingrese la cantidad de ordenes a vender: "<<endl;
+    cin>>cantidad;
+    for (int i = 0; i < cantidad; i++)
+    {
+        cout<<"plato a vender # "<<i+1<<endl;
+        cin>>m.Menu[i];
     }
 }
 int reservacion(){
@@ -46,67 +58,12 @@ int reservacion(){
     cout<<endl;
     return 0;
 }
-void menuDia(string matrizMenu[],string menu[],int precio[]){
-    for (int i = 0; i < 5; i++)
-    {
-        cout<<menu[i]<<" valor LPS."<<precio[i]<<endl;
-    }
-}
-
-int Vender(){
-int matrizMenu();
-    int cantidad;
-    string menu[3];
-    cout<<"Cuantos platillos ordenara:"<<endl;
-    cin>>cantidad;
-    for (int i = 0; i < cantidad; i++)
-    {
-        cout<<"Ingrese el nombre del platillo: "<<endl;
-        cin>>menu[i];
-    }
-    cout<<"Por favor confirme que esta es su orden final y precione ENTER"<<endl;
-    for (int i = 0; i < cantidad; i++)
-    {
-        cout<<menu[i]<<endl;
-    }
-    cout<<"Espere 15 minutos mientras sale su orden"<<endl;
-    cout<<"Aqui esta su orden"<<endl;
-    cout<<"Buen Provecho!!"<<endl;
-   
-	return 0;
-}
-int facturacion(){
-    int numero,Nordenes,factura;
-    string menu[5];
-    int precio[5];
-    cout<<"Ingrese el numero de factura:";
-    cin>>numero;
-    cout<<"Ingrese la cantidad de ordenes que pidio el cliente:"<<endl;
-    cin>>Nordenes;
-    for (int i = 0; i < Nordenes; i++)
-    {
-        cout<<"Ingrese  el nombre de los platillos que consumio el cliente:"<<endl;
-        cin>>menu[i];
-        cout<<"Ingrese el precio que corresponde a ese platillo"<<endl;
-        cin>>precio[i];
- 
-    }
-     cout<<"El cliente consumio:"<<endl;
-    for (int i = 0; i < Nordenes; i++)
-    {
-        cout<<menu[i]<<" con valor de" <<precio[i]<<endl;
-    }
-    cout<<"El total a pagar por el consumo hecho es de:"<<factura<<endl;
-    cout<<"Presione ENTER para imprimir la factura al cliente"<<endl;
-
-    return 0;
-}
 int main(){
     int opcion;
     cout<<endl;
-    cout << "******************************************"<<endl;
-    cout<<"=====BIENVENIDO A RESTAURANTE EL DELFIN====="<<endl;
-    cout<<  "******************************************"<<endl;
+    cout << "***************************************************"<<endl;
+    cout<<"=========BIENVENIDO A RESTAURANTE EL DELFIN========="<<endl;
+    cout<<  "***************************************************"<<endl;
     do
     {
         cout<<endl;
@@ -121,16 +78,15 @@ int main(){
         switch (opcion)
         {
         case 1:
-            cout<<matrizMenu();
+            pedirmenu();
             break;
         case 2:
             cout<<reservacion();
             break;
         case 3:
-           cout<<Vender();
+            mostrarmenu(m1);
             break;
         case 4:
-            cout<<facturacion();
             break;
         default:
         cout<<"Esta opcion no esta disponible"<<endl;
